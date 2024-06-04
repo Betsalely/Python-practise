1ae81347eb1b93cba5f02ecfd4fd73db79650e75
Betsalely <betsalely@wsc.school.nz>
2024-06-04 12:40:43 +1200

    I added collumns and their moveemnt to fallpy bird
    main.py was updated for class

--- Main.py
+++ Main.py
@@ -1,190 +1,214 @@
-def pong():
-    import turtle as t
-    import os
-    player_a_score = 0
-    player_b_score = 0
+# def pong():
+#     import turtle as t
+#     import os
+#     player_a_score = 0
+#     player_b_score = 0
     
-    # create a window and declare a variable called window and call the screen()
-    window = t.Screen()
-    window.title("The Pong Game")
-    window.bgcolor("green")
-    window.setup(width=800, height=600)
-    window.tracer(0)
+#     # create a window and declare a variable called window and call the screen()
+#     window = t.Screen()
+#     window.title("The Pong Game")
+#     window.bgcolor("green")
+#     window.setup(width=800, height=600)
+#     window.tracer(0)
     
-    # Creating the left paddle
-    left_paddle = t.Turtle()
-    left_paddle.speed(0)
-    left_paddle.shape("square")
-    left_paddle.color("white")
-    left_paddle.shapesize(stretch_wid=5, stretch_len=1)
-    left_paddle.penup()
-    left_paddle.goto(-350, 0)
+#     # Creating the left paddle
+#     left_paddle = t.Turtle()
+#     left_paddle.speed(0)
+#     left_paddle.shape("square")
+#     left_paddle.color("white")
+#     left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+#     left_paddle.penup()
+#     left_paddle.goto(-350, 0)
     
-    # Creating the right paddle
-    right_paddle = t.Turtle()
-    right_paddle.speed(0)
-    right_paddle.shape("square")
-    right_paddle.color("white")
-    right_paddle.shapesize(stretch_wid=5, stretch_len=1)
-    right_paddle.penup()
-    right_paddle.goto(350, 0)
+#     # Creating the right paddle
+#     right_paddle = t.Turtle()
+#     right_paddle.speed(0)
+#     right_paddle.shape("square")
+#     right_paddle.color("white")
+#     right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+#     right_paddle.penup()
+#     right_paddle.goto(350, 0)
     
-    # Code for creating the ball
-    ball = t.Turtle()
-    ball.speed(10)
-    ball.shape("circle")
-    ball.color("red")
-    ball.penup()
-    ball.goto(5, 5)
-    ballxdirection = 2
-    ballydirection = 1.5
+#     # Code for creating the ball
+#     ball = t.Turtle()
+#     ball.speed(10)
+#     ball.shape("circle")
+#     ball.color("red")
+#     ball.penup()
+#     ball.goto(5, 5)
+#     ballxdirection = 2
+#     ballydirection = 1.5
     
-    # Code for creating pen for scorecard update
-    pen = t.Turtle()
-    pen.speed(0)
-    pen.color("Blue")
-    pen.penup()
-    pen.hideturtle()
-    pen.goto(0, 260)
-    pen.write("score", align="center", font=('Arial', 24, 'normal'))
+#     # Code for creating pen for scorecard update
+#     pen = t.Turtle()
+#     pen.speed(0)
+#     pen.color("Blue")
+#     pen.penup()
+#     pen.hideturtle()
+#     pen.goto(0, 260)
+#     pen.write("score", align="center", font=('Arial', 24, 'normal'))
     
-    PADDLE_SPEED = 20
+#     PADDLE_SPEED = 20
     
-    def left_paddle_up():
-        y = left_paddle.ycor()
-        y += PADDLE_SPEED
-        if y < 250:  # Adjust this value if needed to limit paddle movement
-            left_paddle.sety(y)
+#     def left_paddle_up():
+#         y = left_paddle.ycor()
+#         y += PADDLE_SPEED
+#         if y < 250:  # Adjust this value if needed to limit paddle movement
+#             left_paddle.sety(y)
 
-    # Function to move the left paddle down smoothly
-    def left_paddle_down():
-        y = left_paddle.ycor()
-        y -= PADDLE_SPEED
-        if y > -240:  # Adjust this value if needed to limit paddle movement
-            left_paddle.sety(y)
+#     # Function to move the left paddle down smoothly
+#     def left_paddle_down():
+#         y = left_paddle.ycor()
+#         y -= PADDLE_SPEED
+#         if y > -240:  # Adjust this value if needed to limit paddle movement
+#             left_paddle.sety(y)
 
-    # Function to move the right paddle up smoothly
-    def right_paddle_up():
-        y = right_paddle.ycor()
-        y += PADDLE_SPEED
-        if y < 250:  # Adjust this value if needed to limit paddle movement
-            right_paddle.sety(y)
+#     # Function to move the right paddle up smoothly
+#     def right_paddle_up():
+#         y = right_paddle.ycor()
+#         y += PADDLE_SPEED
+#         if y < 250:  # Adjust this value if needed to limit paddle movement
+#             right_paddle.sety(y)
 
-    # Function to move the right paddle down smoothly
-    def right_paddle_down():
-        y = right_paddle.ycor()
-        y -= PADDLE_SPEED
-        if y > -240:  # Adjust this value if needed to limit paddle movement
-            right_paddle.sety(y)
-    # Assign keys to play
-    window.listen()
-    window.onkeypress(left_paddle_up, 'w')
-    window.onkeypress(left_paddle_down, 's')
-    window.onkeypress(right_paddle_up, 'Up')
-    window.onkeypress(right_paddle_down, 'Down')
+#     # Function to move the right paddle down smoothly
+#     def right_paddle_down():
+#         y = right_paddle.ycor()
+#         y -= PADDLE_SPEED
+#         if y > -240:  # Adjust this value if needed to limit paddle movement
+#             right_paddle.sety(y)
+#     # Assign keys to play
+#     window.listen()
+#     window.onkeypress(left_paddle_up, 'w')
+#     window.onkeypress(left_paddle_down, 's')
+#     window.onkeypress(right_paddle_up, 'Up')
+#     window.onkeypress(right_paddle_down, 'Down')
 
-    # Exit game on 'q' key press
-    window.onkeypress(window.bye, 'q')
+#     # Exit game on 'q' key press
+#     window.onkeypress(window.bye, 'q')
         
-    while True:
-        window.update()
+#     while True:
+#         window.update()
     
-        # moving the ball
-        ball.setx(ball.xcor() + ballxdirection)
-        ball.sety(ball.ycor() + ballydirection)
+#         # moving the ball
+#         ball.setx(ball.xcor() + ballxdirection)
+#         ball.sety(ball.ycor() + ballydirection)
     
-        # border set up
-        if ball.ycor() > 290:
-            ball.sety(290)
-            ballydirection *=-1
-        if ball.ycor() < -290:
-            ball.sety(-290)
-            ballydirection *= -1
+#         # border set up
+#         if ball.ycor() > 290:
+#             ball.sety(290)
+#             ballydirection *=-1
+#         if ball.ycor() < -290:
+#             ball.sety(-290)
+#             ballydirection *= -1
             
-        if ball.xcor() > 390:
-            ball.goto(0, 0)
-            ballxdirection *= 0
-            ballxdirection += 2
-            player_a_score += 1
-            pen.clear()
-            pen.write("Player A: {}                    Player B: {} ".format(player_a_score, player_b_score), align="center", font=('Monaco', 24, "normal"))
-            os.system("afplay wallhit.wav&")
+#         if ball.xcor() > 390:
+#             ball.goto(0, 0)
+#             ballxdirection *= 0
+#             ballxdirection += 2
+#             player_a_score += 1
+#             pen.clear()
+#             pen.write("Player A: {}                    Player B: {} ".format(player_a_score, player_b_score), align="center", font=('Monaco', 24, "normal"))
+#             os.system("afplay wallhit.wav&")
     
-        if ball.xcor() < -390:  # Left width paddle Border
-            ball.goto(0, 0)
-            ballxdirection *= 0
-            ballxdirection += 2
-            player_b_score += 1
-            pen.clear()
-            pen.write("Player A: {}                    Player B: {} ".format(player_a_score, player_b_score), align="center", font=('Monaco', 24, "normal"))
-            os.system("afplay wallhit.wav&")
+#         if ball.xcor() < -390:  # Left width paddle Border
+#             ball.goto(0, 0)
+#             ballxdirection *= 0
+#             ballxdirection += 2
+#             player_b_score += 1
+#             pen.clear()
+#             pen.write("Player A: {}                    Player B: {} ".format(player_a_score, player_b_score), align="center", font=('Monaco', 24, "normal"))
+#             os.system("afplay wallhit.wav&")
     
-        # Handling the collisions with paddles.
-        if (ball.xcor() > 340) and (ball.xcor() < 350) and (ball.ycor() < right_paddle.ycor() + 40 and ball.ycor() > right_paddle.ycor() - 40):
-            ball.setx(340)
-            ballxdirection *= -1.2
+#         # Handling the collisions with paddles.
+#         if (ball.xcor() > 340) and (ball.xcor() < 350) and (ball.ycor() < right_paddle.ycor() + 40 and ball.ycor() > right_paddle.ycor() - 40):
+#             ball.setx(340)
+#             ballxdirection *= -1.2
             
-            os.system("afplay paddle.wav&")
+#             os.system("afplay paddle.wav&")
     
-        if (ball.xcor() < -340) and (ball.xcor() > -350) and (ball.ycor() < left_paddle.ycor() + 40 and ball.ycor() > left_paddle.ycor() - 40):
-            ball.setx(-340)
-            ballxdirection *= -1.2
+#         if (ball.xcor() < -340) and (ball.xcor() > -350) and (ball.ycor() < left_paddle.ycor() + 40 and ball.ycor() > left_paddle.ycor() - 40):
+#             ball.setx(-340)
+#             ballxdirection *= -1.2
         
 
 
-            os.system("afplay paddle.wav&")
+#             os.system("afplay paddle.wav&")
 
 
 
  
-print('Welcome to Yair Python Quiz')
-answer=input('Are you willing to play the Quiz ? (yes/no) :')
-score=0
-total_questions=3
+# print('Welcome to Yair Python Quiz')
+# answer=input('Are you willing to play the Quiz ? (yes/no) :')
+# score=0
+# total_questions=3
 
-if answer.lower()=='yes':
-    #ask the user and saves it
-    answer=input('Before we start what is your name?')
-    name = answer.lower()
+# if answer.lower()=='yes':
+#     #ask the user and saves it
+#     answer=input('Before we start what is your name?')
+#     name = answer.lower()
 
-    #greets user and introduces quiz
-    answer=input('Ok ' + name+ ':Are you ready to play the Quiz which asks about Python ? (yes/no) :')
-    if answer.lower()=='yes':
-        print('Ok sweet')
-        #ask the user a question
-        answer=input('Question 1: What is the name of the programming language this was coded in? ')
-        if answer.lower()=='python':
-            score += 1
-            print('correct')
-        else:
-            #gives them the correct answer
-            print('Wrong Answer :(, it was Python')
+#     #greets user and introduces quiz
+#     answer=input('Ok ' + name+ ':Are you ready to play the Quiz which asks about Python ? (yes/no) :')
+#     if answer.lower()=='yes':
+#         print('Ok sweet')
+#         #ask the user a question
+#         answer=input('Question 1: What is the name of the programming language this was coded in? ')
+#         if answer.lower()=='python':
+#             score += 1
+#             print('correct')
+#         else:
+#             #gives them the correct answer
+#             print('Wrong Answer :(, it was Python')
             
-        print('Question 2:`how do you write hello in a terminal using python? `')
-        print('A) print(hello)      B) Print(hello)')
-        print('C) print(\'hello\')      D) Print(\"helo\")')
-        answer=input('write one of the letters: ')
-        if answer.lower()=='c':
-            score += 1
-            print('correct')
-        else:
+#         print('Question 2:`how do you write hello in a terminal using python? `')
+#         print('A) print(hello)      B) Print(hello)')
+#         print('C) print(\'hello\')      D) Print(\"helo\")')
+#         answer=input('write one of the letters: ')
+#         if answer.lower()=='c':
+#             score += 1
+#             print('correct')
+#         else:
             
-            print('Wrong Answer :(, it was C) print(\'hello\') s')
+#             print('Wrong Answer :(, it was C) print(\'hello\') s')
 
-        answer = input('Last question before you leave, do you want to play pong yes/no: ')
-        if answer.lower()=='yes':
-            pong()
-        else:
-            print('ok :)')
-    else:
-        print('aw man :(')
-        pong()
+#         answer = input('Last question before you leave, do you want to play pong yes/no: ')
+#         if answer.lower()=='yes':
+#             pong()
+#         else:
+#             print('ok :)')
+#     else:
+#         print('aw man :(')
+#         pong()
         
     
 
-print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
-mark=(score/total_questions)*100
-mark = round(mark)
-print('Marks obtained:',mark)
-print('BYE!')
-#ends the quiz
+# print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
+# mark=(score/total_questions)*100
+# mark = round(mark)
+# print('Marks obtained:',mark)
+# print('BYE!')
+# #ends the quiz
+
+
+
+password = input('password: ')
+
+has_8_letters = False
+
+has_number = False
+
+for char in password:
+    try:
+        num = int(char)
+        has_number = True
+        
+    except ValueError:
+        continue
+
+    if len(password) > 7:
+        has_8_letters = True
+
+if has_number == True and has_8_letters == True:
+    print('valid password')
+else:
+    print('not valid password')
--- flappy_bird.py
+++ flappy_bird.py
@@ -1,7 +1,7 @@
 import pygame
 import numpy as np
 
-col_count = 13
+col_count = 25
 row_count = 18
 
 blue = (0, 0, 200)
@@ -37,16 +37,18 @@ def draw_floor(floor_pattern):
         pygame.draw.rect(screen, color, (col * square_size, (row_count - 1) * square_size, square_size, square_size))
         pygame.draw.rect(screen, black, (col * square_size, (row_count - 1) * square_size, square_size, square_size), 1)
 
-def move_floor(floor_pattern):
+def move_floor(floor_pattern, column_pos, gap_start):
     screen.fill(black)  # Clear the screen before redrawing
     draw_board(board)   # Redraw the board
     
-    # Move floor pattern to the left
+    # Move floor pattern and column position to the left
     floor_pattern = floor_pattern[1:] + floor_pattern[:1]
+    column_pos = (column_pos - 1) % col_count
     
     draw_floor(floor_pattern)
+    draw_pipes(column_pos, gap_start)
     pygame.display.update()
-    return floor_pattern
+    return floor_pattern, column_pos
 
 def possible_up():
     return current_pos > 0
@@ -55,15 +57,15 @@ def draw_player(current_pos, direction):
     if direction == 'UP':
         if possible_up():
             current_pos -= 1
-
     else:
         if current_pos < row_count - 2: 
             current_pos += 1
 
- 
     screen.fill(black)
+
     draw_board(board)
     draw_floor(floor_pattern)
+    draw_pipes(column_pos, gap_start)
     
     pygame.draw.rect(screen, yellow, (6 * square_size, current_pos * square_size, square_size, square_size))
     pygame.draw.rect(screen, black,  (6 * square_size, current_pos * square_size, square_size, square_size), 1)
@@ -71,11 +73,20 @@ def draw_player(current_pos, direction):
 
     return current_pos
 
+def draw_pipes(column_pos, gap_start):
+    for row in range(row_count - 1):
+        if row < gap_start or row >= gap_start + 4:
+            pygame.draw.rect(screen, red, (column_pos * square_size, row * square_size, square_size, square_size))
+            pygame.draw.rect(screen, black, (column_pos * square_size, row * square_size, square_size, square_size), 1)
+
+gap_start = np.random.randint(0, row_count - 5)  # Randomly initialize the gap start position
 floor_pattern = [((col * 18) % 256, 0, 0) for col in range(col_count)]
+column_pos = col_count - 1  # Start column at the rightmost position
 
 board = create_board()
 draw_board(board)
 draw_floor(floor_pattern)
+draw_pipes(column_pos, gap_start)
 
 running = True
 while running:
@@ -89,7 +100,10 @@ while running:
                 current_pos = draw_player(current_pos, "UP")
                 upper = True
 
-    floor_pattern = move_floor(floor_pattern)
+    floor_pattern, column_pos = move_floor(floor_pattern, column_pos, gap_start)
+    if column_pos == col_count - 1:  # Once the column moves back to the rightmost position, reset the gap
+        gap_start = np.random.randint(0, row_count - 5)  # Randomize the gap start position
+
     if not upper:
         current_pos = draw_player(current_pos, "DOWN")
 
